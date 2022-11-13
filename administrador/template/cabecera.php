3bcae7a2b33e4eb12408c8e338e793a781f40301
metoo-col <76023310+metoo-col@users.noreply.github.com>
2022-11-13 18:53:33 -0500

    cambio por que lo anterior fallo

--- administrador/template/cabecera.php
+++ administrador/template/cabecera.php
@@ -10,13 +10,14 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
+    <?php $url="http//".$_SERVER['HTTP_HOST']."/sitioweb" ?>
     <nav class="navbar navbar-expand navbar-light bg-light">
         <div class="nav navbar-nav">
             <a class="nav-item nav-link active" href="#">administrador del sistema <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="#">inicio</a>
-            <a class="nav-item nav-link" href="#">productos</a>
-            <a class="nav-item nav-link" href="#">ver sitio web</a>
+            <a class="nav-item nav-link" href="">inicio</a>
+            <a class="nav-item nav-link" href="#">productos</a>           
             <a class="nav-item nav-link" href="#">cerrar</a>
+            <a class="nav-item nav-link" href="<?php echo $url;?>">ver sitio web</a>
         </div>
     </nav>
     <div class="container">
