65886bed63fd055028543de421d4a8d9e508a522
metoo-col <76023310+metoo-col@users.noreply.github.com>
2022-11-13 19:11:31 -0500

    habia se corrigio enlace

--- administrador/template/cabecera.php
+++ administrador/template/cabecera.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-    <?php $url="http//".$_SERVER['HTTP_HOST']."/sitioweb" ?>
+    <?php $url="http://".$_SERVER['HTTP_HOST']."/sitioweb"?>
     <nav class="navbar navbar-expand navbar-light bg-light">
         <div class="nav navbar-nav">
             <a class="nav-item nav-link active" href="#">administrador del sistema <span class="sr-only">(current)</span></a>
@@ -23,3 +23,4 @@
     <div class="container">
         <br/>
         <div class="row">
+          
