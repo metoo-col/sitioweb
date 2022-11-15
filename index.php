3d6f791972a48d85966f63952a24bdde9e044ca6
metoo-col <76023310+metoo-col@users.noreply.github.com>
2022-11-15 16:01:05 -0500

    cambio tienda.html por index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php include("template/cabecera.php")?>
             <div class="jumbotron text-center">
                 <h1 class="display-3">Bienvenidos a la tienda de golden galaxy</h1>
-                <p class="lead">Aqui podras comprar items que te ayudaran en tu paso poe el servidor</p>
+                <p class="lead">Aqui podras comprar items que te ayudaran en tu paso por el servidor</p>
                 <hr class="my-2">
-                <img width="400" src="img/tienda.png" class="img thumbnail rounded mx-auto d-block "/>
+                <img width="750" src="img/tienda.png" class="img thumbnail rounded mx-auto d-block "/>
                 <p></p>
                 <p class="lead">
                     <a class="btn btn-primary btn-lg" href="productos.php" role="button">Ver productos</a>
