cdac79fafa474c4e31994b6d52bdf444af766aad
metoo-col <76023310+metoo-col@users.noreply.github.com>
2022-11-15 15:50:20 -0500

    cambio

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php include("template/cabecera.php")?>
-            <div class="jumbotron">
-                <h1 class="display-3">Jumbo heading</h1>
-                <p class="lead">Jumbo helper text</p>
+            <div class="jumbotron text-center">
+                <h1 class="display-3">Bienvenidos a la tienda de golden galaxy</h1>
+                <p class="lead">Aqui podras comprar items que te ayudaran en tu paso poe el servidor</p>
                 <hr class="my-2">
-                <p>More info</p>
+                <img width="400" src="img/tienda.png" class="img thumbnail rounded mx-auto d-block "/>
+                <p></p>
                 <p class="lead">
-                    <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+                    <a class="btn btn-primary btn-lg" href="productos.php" role="button">Ver productos</a>
                 </p>
             </div>
 <?php include("template/pie.php")?>
